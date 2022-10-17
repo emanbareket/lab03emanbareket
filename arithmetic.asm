6677a672c37842b18141e3e75474fb6cee26b91b
Immanuel Bareket <ibareket@ucsb.edu>
2022-10-16 21:58:11 -0700

    arithmetic done

---
+++ arithmetic.asm
@@ -0,0 +1,27 @@
+# arithmetic.asm
+# A simple calculator program in MIPS Assembly for CS64
+#
+
+.text
+main:
+    li $v0, 5
+    syscall
+    move $t0, $v0
+    li $v0, 5
+    syscall
+    move $t1, v0
+    sub $t2, $t0, $t1
+    sll $t3, $t2, 4
+    li $t4, 8
+    add $t5, $t3, $t4
+    li $v0, 1
+    move $a0, $t5
+    syscall
+    j exit
+	# TODO: Write your code here
+
+exit:
+    li $v0, 10
+    syscall
+	# Exit SPIM: Write your code here!
+
