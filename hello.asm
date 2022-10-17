2fb3ce2832a42692c1923cbf83c973f5cf8ec2fe
Immanuel Bareket <ibareket@ucsb.edu>
2022-10-16 22:19:44 -0700

    hello take 1

---
+++ hello.asm
@@ -0,0 +1,38 @@
+# hello.asm
+# A "Hello World" program in MIPS Assembly for CS64
+#
+#  Data Area - allocate and initialize variables
+.data
+	# TODO: Write your string definitions here
+    prompt: .asciiz "Enter an integer: "
+
+#Text Area (i.e. instructions)
+.text
+main:
+    li $v0, 4
+    la $a0, promt
+    syscall
+    li $v0, 5
+    syscall
+    move $t0, $v0
+    li $t1, 1
+    and $t2, $t0 $t1
+    beq $t2, $t1, mult_odd
+    mult $t3, $t0, 2
+    li $v0, 1
+    move $a0, $t3
+    syscall
+    j exit
+mult_odd:
+    mult $t3, $t0, 3
+    li $v0, 1
+    move $a0, $t3
+    syscall
+    j exit
+	# TODO: Write your code here
+
+exit:
+    li $v0, 10
+    syscall
+	# Exit SPIM: Write your code here!
+
